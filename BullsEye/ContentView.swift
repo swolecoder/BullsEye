f9aaf51f4ff728e20ff6d63906b6d2e308efd957
swolecoder <swolecoder@gmail.com>
2019-11-07 07:34:04 -0500

    making changes to the UI

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State var alertIsVisible:Bool = false;
     @State var SliderValue: Double = 50;
     @State var target = Int.random(in: 1...100);
+    @State var score = 0;
+    @State var round = 1;
+    
     
     var body: some View {
         
@@ -26,8 +29,7 @@ struct ContentView: View {
             HStack{
                 
                 Text("1")
-                Slider(value: self.$SliderValue, in: 1...100)
-                
+                Slider(value: self.$SliderValue, in: 1...100);
                 Text("100")
             }
             Button(action: {
@@ -38,19 +40,34 @@ struct ContentView: View {
             }
             .alert(isPresented: $alertIsVisible) { () -> Alert in
                 let roundValue: Int = Int(self.SliderValue)
-                return Alert(title: Text("Hello There"), message: Text("Slider Value \(roundValue)"), dismissButton:    .default(Text("Awesone")))
+                return Alert(title: Text("\(self.alertTitle())"), message: Text("Slider Value is \(roundValue) \n"
+                    
+                    + " You scored \(self.pointsForCurrentRound()) points this round"
+                    
+                    ), dismissButton:    .default(Text("Awesone")){
+                        self.score += self.pointsForCurrentRound();
+                        self.target = Int.random(in:1...100);
+                        self.round += 1;
+                        
+                    })
             }
             Spacer()
             HStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.SliderValue = 50;
+                    self.target = Int.random(in: 1...100);
+                    self.score = 0;
+                    self.round = 1;
+                    
+                }) {
                     Text("Start Over")
                 }
                 Spacer()
-                Text("Score")
-                Text("999")
+                Text("Score:")
+                Text("\(self.score)")
                 Spacer()
-                Text("Round")
-                Text("99")
+                Text("Round:")
+                Text("\(self.round)")
                 Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Info")
@@ -59,6 +76,18 @@ struct ContentView: View {
             .padding(.bottom,20)
             
         }
+    }
+    
+    func pointsForCurrentRound()-> (Int){
+        let result = Int(self.SliderValue) - Int(self.target);
+        return 100 - abs(result);
+    }
+    
+    func alertTitle()-> String{
+        let result = Int(self.SliderValue) - Int(self.target);
+        let returnData = result == 0 ? "Perfect" : result < 10 ? "You almost had it!!" : "Keep Trying";
+        return returnData;
+        
     }
 }
 
